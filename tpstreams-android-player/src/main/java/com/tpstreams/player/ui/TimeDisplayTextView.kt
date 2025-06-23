f90ec4de6a8db3f3d44755b49deaf6299538b1af
Gowreesh-A-M <gowreesh@testpress.in>
2025-06-23 18:47:27 +0530

    Rename AutoSizeTimeTextView to TimeDisplayTextView for better naming clarity

--- tpstreams-android-player/src/main/java/com/tpstreams/player/ui/TimeDisplayTextView.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/ui/TimeDisplayTextView.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import androidx.appcompat.widget.AppCompatTextView
 
-class AutoSizeTimeTextView @JvmOverloads constructor(
+class TimeDisplayTextView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
