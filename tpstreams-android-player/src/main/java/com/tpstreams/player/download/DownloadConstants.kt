8b6e8f26f401fd8a3547e67e12becb8d97c95ca8
Gowreesh-A-M <gowreesh@testpress.in>
2025-07-23 11:09:45 +0530

    refactor: Remove unused download constants

--- tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
@@ -4,6 +4,4 @@ object DownloadConstants {
     const val KEY_CUSTOM_METADATA = "customMetadata"
     const val KEY_TITLE = "title"
     const val KEY_THUMBNAIL_URL = "thumbnailUrl"
-    const val KEY_DOWNLOAD_DATE = "downloadDate"
-    const val KEY_CATEGORY = "category"
 } 
