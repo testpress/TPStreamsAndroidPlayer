936f911e2411428e1ec4b09816f2ce814f3e95ba
Syed Ibrahim <syedibrahim@Syeds-Mac-mini.local>
2025-08-01 17:08:39 +0530

    enhacnements

--- tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
@@ -6,7 +6,7 @@ object DownloadConstants {
     const val KEY_THUMBNAIL_URL = "thumbnailUrl"
     const val KEY_CALCULATED_SIZE_BYTES = "calculatedSizeBytes"
 
-    const val FIFTEEN_DAYS_IN_SECONDS = 15 * 24 * 60 * 60
+    const val FIFTEEN_DAYS_IN_SECONDS = 15L * 24 * 60 * 60
 
     const val BITRATE_AUDIO_LOW = 72_000
     const val BITRATE_AUDIO_HIGH = 128_000
