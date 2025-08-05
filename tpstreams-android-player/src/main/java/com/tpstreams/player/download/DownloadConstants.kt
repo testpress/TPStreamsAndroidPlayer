59f8b622599f629d1a56148dca3cd5452d8d3199
Syed Ibrahim <syedibrahim@Syeds-Mac-mini.local>
2025-08-05 17:59:38 +0530

    fix: Add named constant for LicenseExpiral duration

--- tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
@@ -5,4 +5,6 @@ object DownloadConstants {
     const val KEY_TITLE = "title"
     const val KEY_THUMBNAIL_URL = "thumbnailUrl"
     const val KEY_CALCULATED_SIZE_BYTES = "calculatedSizeBytes"
+
+    const val FIFTEEN_DAYS_IN_SECONDS = 15L * 24 * 60 * 60
 } 
