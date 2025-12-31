e45e35124c6a02f2837e70d4f87890bc5f1d631b
syed-tp <syed@testpress.in>
2025-12-31 12:10:46 +0530

    build: release version 1.1.4

--- tpstreams-android-player/build.gradle.kts
+++ tpstreams-android-player/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 }
 apply(from = rootProject.file("gradle/gradle-mvn-build-packages.gradle"))
 group = "com.github.testpress"
-version = "1.1.3"
+version = "1.1.4"
 
 afterEvaluate {
     tasks.findByName("publishReleasePublicationToMavenLocal")?.let { publishTask ->
