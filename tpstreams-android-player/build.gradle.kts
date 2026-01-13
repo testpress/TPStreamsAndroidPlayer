88a3741b3496f5ba8de35922212bcfe568d70c42
syed-tp <syed@testpress.in>
2026-01-13 15:18:10 +0530

    Revert "build: migrate to compilerOptions DSL for Kotlin JVM target"
    This reverts commit e22cf8951fafc6f0a2dd40461da52864ed1924fc.

--- tpstreams-android-player/build.gradle.kts
+++ tpstreams-android-player/build.gradle.kts
@@ -27,10 +27,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
