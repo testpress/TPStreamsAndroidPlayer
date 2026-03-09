87fad7e5b25c7ffb2ecea4e74817ee86d68411b3
Balamurugan <balamurugan@testpress.in>
2026-03-09 18:23:46 +0530

    build: release version 1.1.10

--- tpstreams-android-player/build.gradle.kts
+++ tpstreams-android-player/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 }
 apply(from = rootProject.file("gradle/gradle-mvn-build-packages.gradle"))
 group = "com.github.testpress"
-version = "1.1.9"
+version = "1.1.10"
 
 afterEvaluate {
     tasks.findByName("publishReleasePublicationToMavenLocal")?.let { publishTask ->
