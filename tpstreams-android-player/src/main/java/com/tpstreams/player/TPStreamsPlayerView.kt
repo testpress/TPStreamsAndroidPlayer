33c42646a00f090dcf08854bc8523cb96850fdfd
Balamurugan-Testpress <balamurugan@testpress.in>
2026-05-05 12:14:26 +0530

    remove previous observer before creating new

--- tpstreams-android-player/src/main/java/com/tpstreams/player/TPStreamsPlayerView.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/TPStreamsPlayerView.kt
@@ -352,6 +352,7 @@ class TPStreamsPlayerView @JvmOverloads constructor(
             PlaybackHistoryManager.recordLog(message)
         }
         
+        unregisterFromLifecycle()
         lifecycleManager = player?.let { PlayerLifecycleManager(it) }
         registerWithLifecycle()
         
