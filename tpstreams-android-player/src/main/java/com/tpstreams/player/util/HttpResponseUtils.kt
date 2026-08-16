1c82515aa603b52d9f978dddc587f0773b66e640
Balamurugan-100 <balamurugan@testpress.in>
2026-08-16 10:44:39 +0530

    fix

--- tpstreams-android-player/src/main/java/com/tpstreams/player/util/HttpResponseUtils.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/util/HttpResponseUtils.kt
@@ -7,6 +7,7 @@ internal const val HTTP_STATUS_NOT_FOUND = 404
 internal const val HTTP_STATUS_UNAUTHORIZED = 401
 internal const val HTTP_STATUS_FORBIDDEN = 403
 internal const val HTTP_STATUS_SERVER_ERROR_MIN = 500
+internal const val HTTP_STATUS_SERVER_ERROR_MAX = 599
 
 internal const val DISPLAY_ERROR_ASSET_NOT_FOUND = 5001
 internal const val DISPLAY_ERROR_ACCESS_DENIED = 5002
@@ -31,6 +32,7 @@ internal fun Throwable.isAuthOrContentHttpFailure(): Boolean {
 internal fun Int.toPlaybackErrorFromHttpStatus(): PlaybackError = when (this) {
     HTTP_STATUS_NOT_FOUND -> PlaybackError.INVALID_ASSETS_ID
     HTTP_STATUS_UNAUTHORIZED, HTTP_STATUS_FORBIDDEN -> PlaybackError.INVALID_ACCESS_TOKEN_FOR_ASSETS
+    in HTTP_STATUS_SERVER_ERROR_MIN..HTTP_STATUS_SERVER_ERROR_MAX -> PlaybackError.SERVER_ERROR
     else -> PlaybackError.UNSPECIFIED
 }
 
@@ -39,7 +41,7 @@ internal fun Int.httpStatusUserMessage(playerId: String): String? = when (this) 
         "The video is not available. Please try another one.\n Error code: $DISPLAY_ERROR_ASSET_NOT_FOUND. Player Id: $playerId"
     HTTP_STATUS_UNAUTHORIZED, HTTP_STATUS_FORBIDDEN ->
         "Sorry, you don't have permission to access this video. Please check your credentials and try again.\n Error code: $DISPLAY_ERROR_ACCESS_DENIED. Player Id: $playerId"
-    in HTTP_STATUS_SERVER_ERROR_MIN..599 ->
+    in HTTP_STATUS_SERVER_ERROR_MIN..HTTP_STATUS_SERVER_ERROR_MAX ->
         "We're sorry, but there's an issue on our server. Please try again later.\n Error code: $DISPLAY_ERROR_SERVER. Player Id: $playerId"
     else -> null
 }
