9ff753738388b3bee32906f8ccb5983efe22c8c2
Balamurugan-Testpress <balamurugan@testpress.in>
2026-08-18 11:29:24 +0530

    --amend

--- tpstreams-android-player/src/main/java/com/tpstreams/player/util/WidevinePlaybackLevelResolver.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/util/WidevinePlaybackLevelResolver.kt
@@ -160,9 +160,7 @@ internal object WidevinePlaybackLevelResolver {
                 PlaybackException.ERROR_CODE_DRM_LICENSE_ACQUISITION_FAILED,
                 PlaybackException.ERROR_CODE_DRM_SYSTEM_ERROR,
                 PlaybackException.ERROR_CODE_DRM_DISALLOWED_OPERATION,
-                PlaybackException.ERROR_CODE_DECODER_INIT_FAILED,
-                PlaybackException.ERROR_CODE_DECODING_FAILED,
-                PlaybackException.ERROR_CODE_DECODING_RESOURCES_RECLAIMED -> true
+                PlaybackException.ERROR_CODE_DECODER_INIT_FAILED -> true
                 else -> false
             }
         ) {
