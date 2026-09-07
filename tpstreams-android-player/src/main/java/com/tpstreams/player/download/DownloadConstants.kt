4a0b30d6a3832fbe0f2d030ee9aa6020076cb1f1
Balamurugan-Testpress <balamurugan@testpress.in>
2026-09-07 17:38:11 +0530

    refactor

--- tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
+++ tpstreams-android-player/src/main/java/com/tpstreams/player/download/DownloadConstants.kt
@@ -5,6 +5,7 @@ object DownloadConstants {
     const val KEY_TITLE = "title"
     const val KEY_THUMBNAIL_URL = "thumbnailUrl"
     const val KEY_CALCULATED_SIZE_BYTES = "calculatedSizeBytes"
+    const val KEY_DRM_SECURITY_LEVEL = "drm_security_level"
 
     const val FIFTEEN_DAYS_IN_SECONDS = 15L * 24 * 60 * 60
 } 
